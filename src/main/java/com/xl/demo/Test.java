5cc0c9b357e77ff6eee5111998451d0d6ab31fa2
徐麟 <xu.lin@intellif.com>
2021-05-12 10:43:12 +0800

    commit

--- src/main/java/com/xl/demo/Test.java
+++ src/main/java/com/xl/demo/Test.java
@@ -1,13 +1,91 @@
 package com.xl.demo;
 
-public class Test extends SyncTest{
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
-	public Test(){
-		System.out.println("2");
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class Test{
+	private final KafkaProducer<Integer, String> producer;
+
+	private final Properties props = new Properties();
+
+	private final String topic;
+
+	// Broker地址列表
+	private final String bootstrapServers = "bootstrap.servers";
+
+	// 客户端ID
+	private final String clientId = "client.id";
+
+	// Key序列化类
+	private final String keySerializer = "key.serializer";
+
+	// Value序列化类
+	private final String valueSerializer = "value.serializer";
+
+	// 协议类型:当前支持配置为SASL_PLAINTEXT或者PLAINTEXT
+	private final String securityProtocol = "security.protocol";
+
+	// 服务名
+	private final String saslKerberosServiceName = "sasl.kerberos.service.name";
+
+	// 域名
+	private final String kerberosDomainName = "kerberos.domain.name";
+
+	// 默认发送20条消息
+	// private final int messageNumToSend = 100;
+
+	/**
+	 * 新Producer 构造函数
+	 *
+	 * @param topicName
+	 *            Topic名称
+	 *            是否异步模式发送
+	 */
+	public Test(String topicName) {
+		// Broker地址列表
+		props.put(bootstrapServers, "192.168.11.90:9092");
+		// 客户端ID
+		props.put(clientId, "DemoProducer");
+		// Key序列化类
+		props.put(keySerializer, "org.apache.kafka.common.serialization.IntegerSerializer");
+		// Value序列化类
+		props.put(valueSerializer, "org.apache.kafka.common.serialization.StringSerializer");
+
+		producer = new KafkaProducer<Integer, String>(props);
+		topic = topicName;
+		send();
 	}
-//	@Override
-//	public Integer add(Integer a, Integer b) {
-//		return a+b;
-//	}
+
+	public void send(){
+		Integer num = 1;
+		while (true) {
+			System.out.println("producer is alive");
+			String messageStr = "{\n" +
+					"\t\"id\": \"69894ac944b743ed932fdf8c1693cb3b\",\n" +
+					"\t\"time\": 1553061789000,\n" +
+					"\t\"pointId\": \"abc123456\",\n" +
+					"\t\"captureTime\":"+System.currentTimeMillis()+",\n" +
+					"\t\"faceUrl\": \"http://192.168.11.164//ifsrc/engine1/eng1store1_0/ImgWareHouse/src_0_88212/20190321/20190321T181613_16_88212.jpg\",\n" +
+					"\t\"picUrl\": \"http://192.168.11.164//ifsrc/engine1/eng1store1_0/ImgWareHouse/src_0_88212/20190321/20190321T181613_16_88212.jpg\"\n" +
+					"}";
+			ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>(
+					topic, num, messageStr);
+
+			try {
+				// 同步发送
+				RecordMetadata recordMetadata = producer.send(record).get();
+				if (recordMetadata!=null){
+					System.out.println(recordMetadata.offset());
+				}
+				Thread.sleep(10000L);
+			} catch (Exception ie) {
+				ie.printStackTrace();
+			}
+		}
+	}
 
 }
--- src/main/java/com/xl/demo/TestMain.java
+++ src/main/java/com/xl/demo/TestMain.java
@@ -1,46 +1,70 @@
 package com.xl.demo;
 
+import org.apache.thrift.TException;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Array;
 import java.util.*;
 
 
 public class TestMain {
+
 	public static volatile Boolean  bo = false;
 	public static void main(String[] args) {
-
-		Thread t = new MyThread();
-		t.start();
-		try {
-			Thread.sleep(100L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		t.interrupt();
-		bo = true;
-
-
-		/*List list1 = new LinkedList();
-		List list2 = new ArrayList();
-
-
-		int i = 1;
-		int j = 2;
-		Integer[] a = new Integer[5];
-		a[0] = 7;
-		SyncTest sy = new SyncTest();
-
-		Test t = new Test();*/
-	}
-
-//	default String test(){
-//		System.out.println("123");
-//		return "123";
-//	};
+//        Test test = new Test("face-capture-ytlf");
+		TestStatic test2 = new TestStatic("face-lib-escape");//face-lib-permanent face-lib-escape
+//		try {
+//			CaptureConsumer test = new CaptureConsumer("face-alert");//face-alert
+//		} catch (TException e) {
+//			e.printStackTrace();
+//		}
+//		String addr = "广东省深圳市南山区西乡某镇某村某街道";
+//		String addr = "漳州市芗城区上井56号某某小区1-301";
+//		String[] strings = new String[]{"乡","镇", "路", "村", "街", "道"};
+//		String[] stringNum = new String[]{"1","2", "3", "4", "5", "6","7","8","9","0"};
+//		List<String> stringList = Arrays.asList(strings);
+//		List<String> numList = Arrays.asList(stringNum);
+//		List<Integer> intList = new ArrayList<>();
+//		List<Integer> numIntList = new ArrayList<>();
+//		for (String str : stringList) {
+//			int i = addr.indexOf(str);
+//			if (i != -1){
+//				intList.add(i);
+//			}
+//		}
+//		for (String str : numList) {
+//			int i = addr.indexOf(str);
+//			if (i != -1){
+//				numIntList.add(i);
+//			}
+//		}
+//		Collections.sort(intList);
+//		Collections.sort(numIntList);
+//		if (intList.size() != 0 && numIntList.size() != 0){
+//			if(intList.get(0) < numIntList.get(0)){
+//				addr = addr.substring(0, intList.get(0)+1);
+//			}else{
+//				addr = addr.substring(0, numIntList.get(0));
+//			}
+//		}else if (intList.size() == 0 && numIntList.size() != 0){
+//			addr = addr.substring(0, numIntList.get(0));
+//		}else if(intList.size() != 0 && numIntList.size() == 0){
+//			addr = addr.substring(0, intList.get(0)+1);
+//		}
+//		addr = addr + "XXXXXX";
+//		System.out.println(addr);
+//		for (int i = 0; i < 101; i++) {
+//			BigDecimal bigDecimal = new BigDecimal(i).divide(new BigDecimal(100)).setScale(2, RoundingMode.HALF_UP);
+//			System.out.println(
+//					"INSERT INTO intellif_base.t_rule_info (rule_description,rule_name,thresholds,types) VALUES (\""+i+"\",\""+i+"\",\""+bigDecimal.floatValue()+","+bigDecimal.add(new BigDecimal(0.01)).setScale(2, RoundingMode.HALF_UP).floatValue()+","+bigDecimal.add(new BigDecimal(0.02)).setScale(2, RoundingMode.HALF_UP).floatValue()+"\",\"0,1,2\");"
 //
-//	String test1();
+//			);
+//		}
+	}
 }
